f3b33ceff91f57fc3337bc8ed20961b685ee9a96
khalidhersi <cipherpolfoundation@gmail.com>
2022-01-08 19:16:32 +0000

    mapping each letter

--- src/Compnents/TextAnimation/TextAnimation.jsx
+++ src/Compnents/TextAnimation/TextAnimation.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const TextAnimation = (word) => {
+    const textArr = "I'm".split("");
     return (
-        <Wrapper className="landing__header__title__id">
-            I'm
+        <Wrapper className="landing__header__title__id" >
+            {textArr.map((word, index) => (
+                <span key={index}>{word}</span>
+            ))}
         </Wrapper>
     )
 }
@@ -13,24 +16,43 @@ const TextAnimation = (word) => {
     50& {opacity: 0.5; transorm: translate(-50px) skewY(5deg) skewX(5deg) rotateZ(15deg); filter: blur(5px); }
     100& {opacity: 1; transorm: translate(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px); }
     `
-    const moveInLeft = keyframes`
+    const moveInDown = keyframes`
 0% {
 opacity: 0;
 transform: translateY(-10rem);
+filter: blur(10px);
+skewX(10deg); 
+skewY(10deg);
+rotateZ(30deg);
 }
 
 100% {
 opacity: 1;
 transform: translate(0);
+filter: blur(0px);
+skewX(0deg); 
+skewY(0deg);
+rotateZ(0deg);
 }`
 
 const Wrapper =  styled.h2`
-font-size: 6rem;
-text-transform: uppercase;
-color: #fff;
-font-weight: 300;
-letter-spacing: 0.5rem;
-animation: ${moveInLeft} 1s ease-in-out .3s both;
+    display: inline-block;
+    font-size: 6rem;
+    text-transform: uppercase;
+    color: #fff;
+    font-weight: 300;
+    letter-spacing: 0.5rem;
+    animation: ${moveInDown} 1s ease-in-out .3s both;
+    animation-timing-function: cubic-bezier(0.1, 0.8, 1.5);
+    span:nth-child(1){
+        animation-delay: 0.1s;
+    }
+    span:nth-child(2){
+        animation-delay: 0.8s;
+    }
+    span:nth-child(3){
+        animation-delay: 1.5s;
+    }
 `
 
 export default TextAnimation
