34f95d571e52d19ebc3ef26f3108f8fa6fd31cfc
khalidhersi <cipherpolfoundation@gmail.com>
2022-01-09 14:35:45 +0000

    all other url go to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <SideBar />
 
         <Routes>
-          <Route path="/" element={ <Home />} />
+          <Route path="*" element={ <Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
