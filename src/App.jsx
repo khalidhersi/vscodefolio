98624d79a3929e2a011cead43e0a03ee4ebcd40f
khalidhersi <cipherpolfoundation@gmail.com>
2022-01-09 15:42:44 +0000

    rooting check

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
       <div className="app">
         <Nav />
         <SideBar />
-
         <Routes>
           <Route path="*" element={ <Home />} />
           <Route path="/about" element={<About />} />
