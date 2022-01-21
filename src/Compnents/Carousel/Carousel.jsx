3397e045374729645c93ea46c9e757756bb89a39
khalidhersi <cipherpolfoundation@gmail.com>
2022-01-21 17:13:39 +0000

    demo btn added linking to gh pages of that project

--- src/Compnents/Carousel/Carousel.jsx
+++ src/Compnents/Carousel/Carousel.jsx
@@ -11,6 +11,7 @@ import earWorm from "../../assets/images/ear-worm.png";
 import whiteCross from "../../assets/images/white-cross.png";
 import Button from "../Button/Button";
 import vueClone from "../../assets/images/fullstack-project.png";
+import ticketTracker from "../../assets/images/ticket-tracker.png";
 
 const Carousel = () => {
   const [counter, setCounter] = useState(0);
@@ -42,43 +43,58 @@ const Carousel = () => {
       title: "Iphone Calculator clone",
       description:
         "This is a Iphone Calculator clone I made using vanilla JavaScript. Using DOM manipulation i was able to create a fully functioning calculator that handles basic arithmetic.",
+        GithubLink: "https://khalidhersi.github.io/Calculator/"
     },
     {
       image: minesweeper,
       title: "MineSweeper clone",
       description:
         "I made a MineSweeper clone using vanilla JavaScript. It cointains a Timeer that counts down and ends the game at 0; the game also ends when a bomb is clicked just like the original MineSweeper game.",
-    },
+        GithubLink: "https://khalidhersi.github.io/MineSweeper/"    
+      },
     {
       image: earWorm,
       title: "Ear Worm React App",
       description:
         "This is the first React App I made during a code along in the _nology boot camp. It has Search functionality and a working carousel that lisrts all the Beatles Albums.",
-    },
+        GithubLink: "https://github.com/khalidhersi/react-code-along"
+
+      },
+    {
+      image: ticketTracker,
+      title: "Ticket Tracker",
+      description:
+        "After learning how to immplament the state hook in react, I was able to create a simple Ticket Tracker that has the added bonus of being able to map over a data file of employees and return names + occupations of all the employees in the company.",
+        GithubLink: "https://khalidhersi.github.io/ticket-tracker/"
+      },
     {
       image: punkApi,
       title: "Punk API",
       description:
         "This is a React App that collects data from the Punk API using the fetch(). It displays all the beers availble along with their descriptions and other info.",
-    },
+        GithubLink: "https://khalidhersi.github.io/Punk-API/"
+      },
     {
       image: morseCode,
       title: "Morse Code Translator",
       description:
         "This is a morse Code Translator I made using the test first unit testing approach. I tested using jest allowing me to expect results of the morse code translator function.",
-    },
+        GithubLink: "https://khalidhersi.github.io/Morse-code-translator/"
+      },
     {
       image: clientProject,
       title: "Orchestra of the Age of Enlightenment: Client Project",
       description:
         'This is a client project for the "Orchestra of the Age of Enlightenment"; as a group of 25 we split this project into 2 groups: front-end and back-end; working in an agile team, completing tickets for weekly sprints to produce and showcase the project to the client. My team was working on the front-end side, we created a react app using mobile first design to produce an amazing UI.',
-    },
+        GithubLink: "https://nology-tech.github.io/oae-event-programme/6/home"
+      },
     {
       image: vueClone,
       title: "Full-Stack Project: Vue Clone",
       description:
-        "This fullstack project works using a java backend I created that conatins a list of movies and thier data. The Front-end collects the Back-ends data using fetch; you can navigate to the differnt endpoints of the backend using differnt urls. This backend supports GET requests, POST requests and DELETE requests and was modeled after the Vue cinema Front-end Design."
-    },
+        "This fullstack project works using a java backend I created that conatins a list of movies and thier data. The Front-end collects the Back-ends data using fetch; you can navigate to the differnt endpoints of the backend using differnt urls. This backend supports GET requests, POST requests and DELETE requests and was modeled after the Vue cinema Front-end Design.",
+        GithubLink: "https://khalidhersi.github.io/React-FrontEnd/"
+      },
   ];
 
   const buttonJSX = (
@@ -103,6 +119,7 @@ const Carousel = () => {
       <h3 className="carousel__content--text__heading">
         {projectInfoArr[counter].title}
       </h3>
+      <a href={projectInfoArr[counter].GithubLink} target="_blank"><Button buttonText={"Demo"} isSecondary={false} /></a>
       <p className="carousel__content--text__description">
         {projectInfoArr[counter].description}
       </p>
