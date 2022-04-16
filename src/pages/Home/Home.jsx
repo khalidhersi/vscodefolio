786b8670e90d94dfe8b9879c2296b556a03012e7
khalidhersi <cipherpolfoundation@gmail.com>
2022-04-16 23:06:31 +0100

    p changed to div on home page typewriter component to resolve dom error div cannot descend fro m a p tag

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <TextAnimation />
         <NameTextAnimation />
         <LastNameAnimation />
-        <p className="landing__header__desc">
+        <div className="landing__header__desc">
           <span className="text-accent">
             <Typewriter
               onInit={(typewriter) => {
@@ -38,7 +38,7 @@ const Home = () => {
               typewriter.pauseFor(8700).typeString("based in London").start();
             }}
           />
-        </p>
+          </div>
       </header>
     </section>
   );
